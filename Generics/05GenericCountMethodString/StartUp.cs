406795fc024f35a7dafce15afd06d19c480d32af
AtanasovTsvetomir <cwetomirrrr@gmail.com>
2019-08-07 11:32:04 +0300

    Added Generics exercise.

---
+++ Generics/05GenericCountMethodString/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfBoxes = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < numberOfBoxes; i++)
+            {
+                string number = Console.ReadLine();
+                box.Add(number);
+            }
+
+            string elementToCompare = Console.ReadLine();
+
+            Console.WriteLine(box.Count(elementToCompare));
+        }
+    }
+}
